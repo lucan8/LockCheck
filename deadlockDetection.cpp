100d890293a1b17c8f01db30085ac14acc9bda1b
Mihnea Cucu <68858101+MihneaCucu@users.noreply.github.com>
2025-01-14 23:04:57 +0200

    Update deadlockDetection.cpp

--- deadlockDetection.cpp
+++ deadlockDetection.cpp
@@ -4,6 +4,7 @@
 #include <regex>
 #include <unordered_map>
 #include <unordered_set>
+
 class DeadlockDetector {
 public:
     void detectDeadlock(const std::string& filename) {
@@ -16,25 +17,30 @@ public:
         std::string line;
         std::unordered_map<std::string, std::unordered_set<std::string>> lockGraph;
         std::unordered_map<std::string, std::unordered_set<std::string>> threadLocks;
-        std::regex lockRegex(R"(pthread_mutex_lock\(\s*&(\w+)\s*\))");
-        std::regex unlockRegex(R"(pthread_mutex_unlock\(\s*&(\w+)\s*\))");
+        std::regex lockMutexRegex(R"(pthread_mutex_lock\(\s*&(\w+)\s*\))");
+        std::regex unlockMutexRegex(R"(pthread_mutex_unlock\(\s*&(\w+)\s*\))");
+        std::regex lockSemRegex(R"(sem_wait\(\s*&(\w+)\s*\))");
+        std::regex unlockSemRegex(R"(sem_post\(\s*&(\w+)\s*\))");
 
         std::string currentThread = "main";
         while (std::getline(file, line)) {
             std::smatch match;
-            if (std::regex_search(line, match, lockRegex)) {
-                std::string mutexName = match[1].str();
-                if (!threadLocks[currentThread].empty()) {
-                    for (const auto& heldMutex : threadLocks[currentThread]) {
-                        lockGraph[heldMutex].insert(mutexName);
-                    }
-                }
-                threadLocks[currentThread].insert(mutexName);
-            }
 
-            if (std::regex_search(line, match, unlockRegex)) {
+            if (std::regex_search(line, match, lockMutexRegex)) {
                 std::string mutexName = match[1].str();
-                threadLocks[currentThread].erase(mutexName);
+                handleLock(threadLocks, lockGraph, currentThread, mutexName);
+            }
+            if (std::regex_search(line, match, unlockMutexRegex)) {
+                std::string mutexName = match[1].str();
+                handleUnlock(threadLocks, currentThread, mutexName);
+            }
+            if (std::regex_search(line, match, lockSemRegex)) {
+                std::string semName = match[1].str();
+                handleLock(threadLocks, lockGraph, currentThread, semName);
+            }
+            if (std::regex_search(line, match, unlockSemRegex)) {
+                std::string semName = match[1].str();
+                handleUnlock(threadLocks, currentThread, semName);
             }
         }
 
@@ -48,6 +54,24 @@ public:
     }
 
 private:
+    void handleLock(std::unordered_map<std::string, std::unordered_set<std::string>>& threadLocks,
+                    std::unordered_map<std::string, std::unordered_set<std::string>>& lockGraph,
+                    const std::string& currentThread,
+                    const std::string& resourceName) {
+        if (!threadLocks[currentThread].empty()) {
+            for (const auto& heldResource : threadLocks[currentThread]) {
+                lockGraph[heldResource].insert(resourceName);
+            }
+        }
+        threadLocks[currentThread].insert(resourceName);
+    }
+
+    void handleUnlock(std::unordered_map<std::string, std::unordered_set<std::string>>& threadLocks,
+                      const std::string& currentThread,
+                      const std::string& resourceName) {
+        threadLocks[currentThread].erase(resourceName);
+    }
+
     bool hasCycle(const std::unordered_map<std::string, std::unordered_set<std::string>>& graph) {
         std::unordered_map<std::string, bool> visited;
         std::unordered_map<std::string, bool> inStack;
