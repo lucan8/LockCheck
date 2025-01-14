e7efb5f9b6b53dd3369e426b70cddaba8ec57083
Mihnea Cucu <68858101+MihneaCucu@users.noreply.github.com>
2025-01-14 22:50:34 +0200

    Update deadlockDetection.cpp

--- deadlockDetection.cpp
+++ deadlockDetection.cpp
@@ -1,110 +1,50 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <regex>
-#include <unordered_map>
-#include <stack>
-#include <set>
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
 
-class DeadlockDetector {
-public:
-    DeadlockDetector() {}
+pthread_mutex_t mutex1;
+pthread_mutex_t mutex2;
 
-    void detectDeadlock(const std::string& filename) {
-        std::ifstream file(filename);
-        if (!file.is_open()) {
-            std::cerr << "Eroare la deschiderea fisierului." << std::endl;
-            return;
-        }
+void* thread1_func(void* arg) {
+    pthread_mutex_lock(&mutex1);
+    printf("Thread 1 a blocat mutex1\n");
+    sleep(1); // Simulează întârziere, crescând probabilitatea de deadlock
+    pthread_mutex_lock(&mutex2);
+    printf("Thread 1 a blocat mutex2\n");
 
-        std::string line;
-        std::unordered_map<std::string, std::set<std::string>> lockGraph;
-        std::unordered_map<std::string, bool> mutexLocked;
-        std::unordered_map<std::string, std::stack<std::string>> threadLocks;
+    pthread_mutex_unlock(&mutex2);
+    pthread_mutex_unlock(&mutex1);
 
-        std::regex mutexRegex(R"(pthread_mutex_t\s+(\w+);)");
-        std::regex lockCallRegex(R"(pthread_mutex_lock\(\s*&(\w+)\s*\))");
-        std::regex unlockCallRegex(R"(pthread_mutex_unlock\(\s*&(\w+)\s*\))");
+    return NULL;
+}
 
-        while (std::getline(file, line)) {
-            std::smatch match;
+void* thread2_func(void* arg) {
+    pthread_mutex_lock(&mutex2);
+    printf("Thread 2 a blocat mutex2\n");
+    sleep(1); // Simulează întârziere, crescând probabilitatea de deadlock
+    pthread_mutex_lock(&mutex1);
+    printf("Thread 2 a blocat mutex1\n");
 
-            if (std::regex_search(line, match, mutexRegex)) {
-                std::string mutexName = match[1];
-                mutexLocked[mutexName] = false;
-            }
+    pthread_mutex_unlock(&mutex1);
+    pthread_mutex_unlock(&mutex2);
 
-
-            if (std::regex_search(line, match, lockCallRegex)) {
-                std::string mutexName = match[1];
-                std::string threadName = "Thread_" + std::to_string(threadId);
-
-                if (mutexLocked[mutexName]) {
-                    lockGraph[mutexName].insert(threadName);
-                } else {
-                    mutexLocked[mutexName] = true;
-                    threadLocks[threadName].push(mutexName);
-                }
-            }
-
-            if (std::regex_search(line, match, unlockCallRegex)) {
-                std::string mutexName = match[1];
-                std::string threadName = "Thread_" + std::to_string(threadId);
-
-                if (!threadLocks[threadName].empty() && threadLocks[threadName].top() == mutexName) {
-                    threadLocks[threadName].pop();
-                    mutexLocked[mutexName] = false;
-                }
-            }
-        }
-
-        if (hasCycle(lockGraph)) {
-            std::cout << "Posibilitate de deadlock detectata!" << std::endl;
-        } else {
-            std::cout << "Nu s-a detectat deadlock." << std::endl;
-        }
-
-        file.close();
-    }
-
-private:
-    bool hasCycle(const std::unordered_map<std::string, std::set<std::string>>& graph) {
-        std::unordered_map<std::string, bool> visited;
-        std::unordered_map<std::string, bool> inStack;
-
-        for (const auto& node : graph) {
-            if (!visited[node.first]) {
-                if (dfs(node.first, graph, visited, inStack)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    bool dfs(const std::string& node, const std::unordered_map<std::string, std::set<std::string>>& graph,
-             std::unordered_map<std::string, bool>& visited, std::unordered_map<std::string, bool>& inStack) {
-        visited[node] = true;
-        inStack[node] = true;
-
-        for (const auto& neighbor : graph.at(node)) {
-            if (!visited[neighbor] && dfs(neighbor, graph, visited, inStack)) {
-                return true;
-            } else if (inStack[neighbor]) {
-                return true;
-            }
-        }
-
-        inStack[node] = false;
-        return false;
-    }
-
-    int threadId = 0;
-};
+    return NULL;
+}
 
 int main() {
-    DeadlockDetector detector;
-    detector.detectDeadlock("exemplu.C");
+    pthread_mutex_init(&mutex1, NULL);
+    pthread_mutex_init(&mutex2, NULL);
+
+    pthread_t thread1, thread2;
+
+    pthread_create(&thread1, NULL, thread1_func, NULL);
+    pthread_create(&thread2, NULL, thread2_func, NULL);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    pthread_mutex_destroy(&mutex1);
+    pthread_mutex_destroy(&mutex2);
+
     return 0;
 }
